b04d5e16aa8921934b273a538a84eca5ce22cd2c
shangxinyi <65947189+shangxinyi@users.noreply.github.com>
2022-01-14 16:06:30 +0800

    Create long_tailed_cifar10.py

---
+++ Dataset/long_tailed_cifar10.py
@@ -0,0 +1,37 @@
+import numpy as np
+from Dataset.dataset import label_indices2indices
+import copy
+
+
+def _get_img_num_per_cls(list_label2indices_train, num_classes, imb_factor, imb_type):
+    img_max = len(list_label2indices_train) / num_classes
+    img_num_per_cls = []
+    if imb_type == 'exp':
+        for _classes_idx in range(num_classes):
+            num = img_max * (imb_factor**(_classes_idx / (num_classes - 1.0)))
+            img_num_per_cls.append(int(num))
+    return img_num_per_cls
+
+
+def train_long_tail(list_label2indices_train, num_classes, imb_factor, imb_type):
+    new_list_label2indices_train = label_indices2indices(copy.deepcopy(list_label2indices_train))
+    img_num_list = _get_img_num_per_cls(copy.deepcopy(new_list_label2indices_train), num_classes, imb_factor, imb_type)
+    print('img_num_class')
+    print(img_num_list)
+
+    list_clients_indices = []
+    classes = list(range(num_classes))
+    for _class, _img_num in zip(classes, img_num_list):
+        indices = list_label2indices_train[_class]
+        np.random.shuffle(indices)
+        idx = indices[:_img_num]
+        list_clients_indices.append(idx)
+    num_list_clients_indices = label_indices2indices(list_clients_indices)
+    print('All num_data_train')
+    print(len(num_list_clients_indices))
+    return img_num_list, list_clients_indices
+
+
+
+
+
